eae9f1872aea3841a21ab393f47f1711d29bf3c8
benjamin.haddleton <benjamin.haddleton@CPX-TQHOXM5SECE>
2020-11-16 13:50:12 +0000

    Intial Commit

---
+++ PowershellProfile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,78 @@
+if (!(Get-Module -ListAvailable -Name posh-git)) {
+    try {
+        Install-Module -Name posh-git -Confirm:$false -Force
+    }
+    catch [Exception] {
+        $_.message 
+        exit
+    }
+}
+
+if (!(Get-Module -ListAvailable -Name oh-my-posh)) {
+    try {
+        Install-Module -Name oh-my-posh -Confirm:$false -Force
+    }
+    catch [Exception] {
+        $_.message 
+        exit
+    }
+}
+
+if (!(Get-Module -ListAvailable -Name Terminal-Icons)) {
+    try {
+        Install-Module -Name Terminal-Icons -Confirm:$false -Force
+    }
+    catch [Exception] {
+        $_.message 
+        exit
+    }
+}
+
+Import-Module -Name posh-git
+Import-Module -Name oh-my-posh
+Import-Module -Name Terminal-Icons
+
+Write-Host "                                ..," -ForegroundColor DarkCyan
+Write-Host "                    ....,,:;+ccllll" -ForegroundColor DarkCyan
+Write-Host "      ...,,+:;  cllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host ",cclllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host ""
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "llllllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "``'ccllllllllll  lllllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "       ``'\\*::  :ccllllllllllllllll" -ForegroundColor DarkCyan
+Write-Host "                       ````````''*::cll" -ForegroundColor DarkCyan
+Write-Host "                                 ````" -ForegroundColor DarkCyan
+
+Set-Theme Agnoster
+
+# Import-Module MSTerminalSettings
+# $msTermProfileName = 'PowerShell' # Replace with whatever Terminal profile name you're using
+# $msTermProfile     = Get-MSTerminalProfile -Name $msTermProfileName
+# $script:bombThrown = $false
+# function prompt {
+#     if ($? -eq $false) {
+#         # RED ALERT!!!
+#         # Only do this if we're using Microsoft Terminal
+#         if ((Get-Process -Id $PID).Parent.ProcessName -eq 'WindowsTerminal') {
+#             Set-MSTerminalProfile -Name $msTermProfile.name -BackgroundImage '%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState\oh-no.gif' -UseAcrylic:$false -BackgroundImageStretchMode "fill" -BackgroundImageAlignment "center"
+#             $script:bombThrown = $true
+#         }
+#     } else {
+#         # Reset to previous settings
+#         if ($script:bombThrown) {
+#             Set-MSTerminalProfile -Name $msTermProfile.name -BackgroundImage $msTermProfile.backgroundImage -UseAcrylic:$msTermProfile.useAcrylic -BackgroundImageStretchMode $msTermProfile.backgroundImageStretchMode -BackgroundImageAlignment $msTermProfile.backgroundImageAlignment
+#             $script:bombThrown = $false
+#         }
+#     }
+# }
+
+
